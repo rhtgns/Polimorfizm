abcf9283b5e6c70e6019ae91585000b8f863e30c
rohat güneş <rohatgns44@gmail.com>
2024-08-13 14:24:03 +1000

    Add project files.

---
+++ Polimorfizm/BaseGeometrikSekil.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizm
+{
+    public class BaseGeometrikSekil
+    {
+
+        // Genişlik ve Yükseklik propertyleri tanımlıyoruz
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        // Sanal (virtual) AlanHesapla  burda alanı hesaplayan method tanımlıyoruz
+        public virtual double CalculateField()
+        {
+            return Width * Height;
+        }
+
+
+        public class Square : BaseGeometrikSekil
+        {
+
+            public override double CalculateField()
+            {
+                return Width * Height;
+            }
+
+        }
+        public class Dikdortgen : BaseGeometrikSekil
+        {
+            // Alan hesaplama, BaseGeometrikSekil'deki aynı yontemi kullandık
+            public override double CalculateField()
+            {
+                return base.CalculateField();
+            }
+        }
+
+        public class Dikucgen : BaseGeometrikSekil
+        {
+
+            public override double CalculateField()
+            {
+                return (Width * Height) / 2;
+            }
+        }
+
+    }
+}
---
+++ Polimorfizm/Program.cs
@@ -0,0 +1,32 @@
+﻿using static Polimorfizm.BaseGeometrikSekil;
+
+class BaseGeometrikSekil
+{
+    static void Main(string[] args)
+    {
+
+        // simdi bir kare nesnesi  oluşturalım
+
+        Square square = new Square
+        {
+            Height = 15,
+            Width = 10,
+        };
+
+        Dikdortgen dikdortgen = new Dikdortgen
+        {
+
+            Height = 20,
+            Width = 10,
+        };
+        Dikucgen dikucgen = new Dikucgen
+        {
+            Height = 25,
+            Width = 40,
+        };
+        // şimdi de her nesne icin konsola  yazdralım
+        Console.WriteLine($"kare alanı : {square.CalculateField()}");
+        Console.WriteLine($"dıkdortgen alanı : {dikdortgen.CalculateField()}");
+        Console.WriteLine($"dık ucgen alanı :{dikucgen.CalculateField()}");
+    }
+}
